a3b98d171e9f1828936d8dccf3214bd2e140793e
Daniel Baumgarten <incognym@googlemail.com>
2020-06-13 20:00:59 +0200

    Fix github-release changelogging

--- build-changelog.sh
+++ build-changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "## Changes since $(git describe --tags --abbrev=0 @^) (auto-generated)" > CHANGELOG.md
+echo "## Changes since $(git describe --tags --abbrev=0 @^) (auto-generated)"  > CHANGELOG.md
 git log '--format=format: - %s' $(git describe --tags --abbrev=0 @^)..@ >> CHANGELOG.md
 echo "" >> CHANGELOG.md
 cat CHANGELOG.md
