2e4baa929f9535d82138d0b0e31abc9b02e7ff7e
Daniel Baumgarten <incognym@googlemail.com>
2020-11-11 20:20:07 +0100

    Made build-docs.sh work on windows

--- build-docs.sh
+++ build-docs.sh
@@ -5,6 +5,16 @@ set -e
 # It is automatically run by the ci-tool
 # This way, duplicatin between repo and documentation is reduced massively and the documentation is automatically kept up-to-date.
 
+
+if [ -f "./yodk" ]; then
+    YODK_BINARY="./yodk"
+elif [ -f "./yodk.exe" ]; then
+    YODK_BINARY="./yodk.exe"
+else
+    echo No compiled yodk binary found
+    exit 1
+fi
+
 rm -rf docs/generated || true
 mkdir -p docs/generated/code/yolol
 mkdir -p docs/generated/code/nolol
@@ -15,14 +25,14 @@ cp examples/yolol/*.yolol docs/generated/code/yolol
 cp examples/nolol/*.nolol docs/generated/code/nolol
 cp examples/yolol/fizzbuzz_test.yaml docs/generated/tests
 
-./yodk compile docs/generated/code/nolol/*.nolol
-./yodk format docs/generated/code/nolol/*.nolol
-./yodk format docs/generated/code/yolol/*.yolol
-./yodk optimize docs/generated/code/yolol/unoptimized.yolol
+${YODK_BINARY} compile docs/generated/code/nolol/*.nolol
+${YODK_BINARY} format docs/generated/code/nolol/*.nolol
+${YODK_BINARY} format docs/generated/code/yolol/*.yolol
+${YODK_BINARY} optimize docs/generated/code/yolol/unoptimized.yolol
 
 cp vscode-yolol/README.md docs/vscode-yolol.md
 cp README.md docs/README.md
 sed -i 's/https:\/\/dbaumgarten.github.io\/yodk\/#//g' docs/README.md
-echo "help" | ./yodk debug examples/yolol/fizzbuzz.yolol | grep -v EOF > docs/generated/cli/debug-help.txt
+echo "help" | ${YODK_BINARY} debug examples/yolol/fizzbuzz.yolol | grep -v EOF > docs/generated/cli/debug-help.txt
 
 chmod -R a+r docs
