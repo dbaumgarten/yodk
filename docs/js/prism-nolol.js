77914e90b9a8ae8c8e0e25b3f4d0facf71ffa4d4
dbaumgarten <mail@dbaumgarten.net>
2020-12-22 00:27:31 +0100

    Fixed highlighting of jump-labels

--- docs/js/prism-nolol.js
+++ docs/js/prism-nolol.js
@@ -1,7 +1,7 @@
 Prism.languages.nolol = {
     'comment': /\/\/.+/,
     'string': /"[^"]*"/,
-    'tag': /[a-zA-Z]+[a-zA-Z0-9_]*>/,
+    'tag': /^\s*[a-zA-Z]+[a-zA-Z0-9_]*>/,
     'keyword': /\b(if|then|else|end|goto|define|while|do|wait|include|macro|insert|break|continue|_if|_goto)\b/i,
     'operator': /\b(and|or|not)\b/i,
     'function': /[a-z0-9_]+(?=\()/i,
