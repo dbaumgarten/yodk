e5d99d788f58ec869d9c2792b21ce65010c2cda8
dbaumgarten <mail@dbaumgarten.net>
2021-04-06 00:12:25 +0200

    Improved changelog-generation

--- build-changelog.sh
+++ build-changelog.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
-echo "## Changes since $(git describe --tags --abbrev=0 @^) (auto-generated)"  > CHANGELOG.md
-git log '--format=format: - %s' $(git describe --tags --abbrev=0 @^)..@ >> CHANGELOG.md
-echo "" >> CHANGELOG.md
+TAGS=`git tag | sort -V -r | head -n 10`
+TAG1=""
+TAG2=""
+echo -e "# Auto-generated changelog\n\n" > CHANGELOG.md
+while read line; do 
+    TAG1=$TAG2
+    TAG2=$line
+    if [ "$TAG1" != "" ] ; then
+        echo "## $TAG1" >> CHANGELOG.md
+        git log '--format=format: - %s' $TAG2..$TAG1 >> CHANGELOG.md
+        echo -e "\n" >> CHANGELOG.md
+    fi
+done <<< "$TAGS"
+
 cat CHANGELOG.md
