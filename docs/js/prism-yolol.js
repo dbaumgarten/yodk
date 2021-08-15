31bc87ecec8b30f7877830b7efd71decc40563c3
dbaumgarten <mail@dbaumgarten.net>
2021-08-15 12:53:25 +0200

    Improved syntax-highlighting for weird field-names

--- docs/js/prism-yolol.js
+++ docs/js/prism-yolol.js
@@ -4,6 +4,6 @@ Prism.languages.yolol = {
     'keyword': /((?<=^|\\s|[^a-zA-Z_:])(if|then|else|end|goto)+)|(?<=^|\\s|[^a-zA-Z0-9_:])(not|abs|sqrt|sin|cos|tan|asin|acos|atan)(?=[^a-zA-Z0-9_:]|$)/i,
     'operator': /(?<=[^a-zA-Z0-9_:])(and|or)(?=[^a-zA-Z0-9_:])/i,
     'function': /[a-z0-9_]+(?=\()/i,
-    'variable': /:?[a-zA-Z]+[a-zA-Z0-9_]*/,
+    'variable': /:[a-zA-Z0-9_]+|^[a-zA-Z]+[a-zA-Z0-9_]*/,
     'constant': /[0-9]+(\.[0-9]+)?/
 };
