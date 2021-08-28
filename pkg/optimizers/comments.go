8a20e19160b956d2b0b3a9084a178c8b58a71884
dbaumgarten <mail@dbaumgarten.net>
2021-08-28 14:35:54 +0200

    Remove trailing empty lines when optimizing #96

--- pkg/optimizers/comments.go
+++ pkg/optimizers/comments.go
@@ -10,7 +10,24 @@ type CommentOptimizer struct {
 
 // Optimize is needed to implement Optimizer
 func (o *CommentOptimizer) Optimize(prog ast.Node) error {
-	return prog.Accept(o)
+	err := prog.Accept(o)
+	if err != nil {
+		return err
+	}
+
+	// remove trailing empty lines
+	if prog, is := prog.(*ast.Program); is {
+		for i := len(prog.Lines) - 1; i >= 0; i-- {
+			hasStatements := len(prog.Lines[i].Statements) > 0
+			if !hasStatements {
+				prog.Lines = prog.Lines[:i]
+			} else {
+				break
+			}
+		}
+	}
+
+	return nil
 }
 
 // Visit is needed to implement Visitor
